abc034e2e04463d992e8bd4ec0a0f08a0aa97ce1
Natasha Klingenbrunn <natasha.klingenbrunn@gmail.com>
2021-08-04 23:50:46 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
 const { app, BrowserWindow } = require('electron')
+const {PythonShell} = require('python-shell');
+const path = require('path')
+
+
 
 function createWindow () {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600
-    })
-  
-    win.loadFile('index.html')
-  }
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
 
+  win.loadFile('index.html')
+
+  var python = require('child_process').spawn('python', ['./script.py']);
+  python.stdout.on('data',function(data){
+      console.log("data: ",data.toString('utf8'));
+  });
+
+}
+
+// Quit the app when all windows are closed (Windows & Linux)
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+
+// Open a window if none are open (macOS)
 app.whenReady().then(() => {
   createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+  
 })
+
