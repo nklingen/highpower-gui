42d117b5e7173e89ebac4b18084355095dff960d
nklingen <natasha.klingenbrunn@gmail.com>
2021-08-23 15:06:42 +0200

    formatting

--- 4GruppeHP.py
+++ 4GruppeHP.py
@@ -7,19 +7,27 @@ import adafruit_ina260
 import pigpio
 import sys
 import csv
-from collections import deque 
+from collections import deque
 
+def water_switch(current_switch, target_val):
+    if current_switch == 1:
+        target_val = 0
+    return target_val
+    
 def update_duty_cycle(duty_val, current_val, target_val):
     if int(current_val) < int(target_val) :
         duty_val += 0.01
     elif int(current_val) > int(target_val) :
         duty_val -= 0.01
-        
+
     # Duty max is 100 and min is 0
-    if duty_val <= 0 :
-        duty_val = 0  
-    elif duty_val >= 100 :
-        duty_val = 100
+    duty_val = max(0, duty_val)
+    duty_val = min(100, duty_val)
+
+    # If the target value has been changed to 0  immediately change duty to 0
+    if int(target_val) == 0 :
+        duty_val = 0
+        
     return duty_val
 
 def run(mv1,mv2,mv3,mv4):
@@ -28,22 +36,16 @@ def run(mv1,mv2,mv3,mv4):
     queue2 = deque()
     queue3 = deque()
     queue4 = deque()
-    #test connection with screen
-    # f = open("demofile2.txt", "a")
-    # f.write(mv1)
-    # f.write(mv2)
-    # f.write(mv3)
-    # f.write(mv4)
-    # f.close()
 
     #_____________________________________________________________________________________________
     # Setup
 
     # Initialize voltage sensors
-    ina260_1 = adafruit_ina260.INA260(0x40)
-    ina260_2 = adafruit_ina260.INA260(0x41)
-    ina260_3 = adafruit_ina260.INA260(0x44)
-    ina260_4 = adafruit_ina260.INA260(0x43)
+    i2c = busio.I2C(board.SCL, board.SDA)
+    ina260_1 = adafruit_ina260.INA260(i2c, address = 0x40)
+    ina260_2 = adafruit_ina260.INA260(i2c, address = 0x41)
+    ina260_3 = adafruit_ina260.INA260(i2c, address = 0x44)
+    ina260_4 = adafruit_ina260.INA260(i2c, address = 0x45)
 
     # Initialize dokument for logbook
     date = datetime.datetime.now().strftime('%m-%d-%Y_%H.%M.%S')
@@ -54,11 +56,17 @@ def run(mv1,mv2,mv3,mv4):
     # Tell the library which pin nunbering system you are going to use
     GPIO.setmode(GPIO.BCM)
 
-    # Initialize pins
+    # Initialize pwm pins
     GPIO.setup(12, GPIO.OUT)
     GPIO.setup(13, GPIO.OUT)
     GPIO.setup(19, GPIO.OUT)
     GPIO.setup(16, GPIO.OUT)
+    
+    # Initialize switch pins
+    switch1 = GPIO.setup(17, GPIO.IN)
+    switch2 = GPIO.setup(27, GPIO.IN)
+    switch3 = GPIO.setup(22, GPIO.IN)
+    switch4 = GPIO.setup(10, GPIO.IN)
 
     # Initialize variables 
     duty1 = 0
@@ -95,18 +103,22 @@ def run(mv1,mv2,mv3,mv4):
     while 1:
         
         # Group 1
+        tagtet_voltage1 = water_switch(GPIO.input(17), tagtet_voltage1)
         duty1 = update_duty_cycle(duty1, ina260_1.voltage*1000, tagtet_voltage1)
         Gruppe1.ChangeDutyCycle(duty1)
         
         # Group 2
+        tagtet_voltage2 = water_switch(GPIO.input(27), tagtet_voltage2)
         duty2 = update_duty_cycle(duty2, ina260_2.voltage*1000, tagtet_voltage2)
         Gruppe2.ChangeDutyCycle(duty2)
         
         # Group 3
+        tagtet_voltage3 = water_switch(GPIO.input(22), tagtet_voltage3)
         duty3 = update_duty_cycle(duty3, ina260_3.voltage*1000, tagtet_voltage3)
         Gruppe3.ChangeDutyCycle(duty3)
         
         # Group 4
+        tagtet_voltage4 = water_switch(GPIO.input(10), tagtet_voltage4)
         duty4 = update_duty_cycle(duty4, ina260_4.voltage*1000, tagtet_voltage4)
         Gruppe4.ChangeDutyCycle(duty4)
 
@@ -146,7 +158,7 @@ def run(mv1,mv2,mv3,mv4):
 
 
         # After 1000 loops update the csv file
-        i = i + 1
+        i += 1
         if i == 1000:
             
             avg_1 = sum(queue1)/50
@@ -161,7 +173,7 @@ def run(mv1,mv2,mv3,mv4):
             # create the csv writer
             writer = csv.writer(f)
             # write a row to the csv file
-            writer.writerow((avg_1*1000), avg_2*1000,avg_3*1000,avg_4*1000))
+            writer.writerow(avg_1*1000, avg_2*1000,avg_3*1000,avg_4*1000)
             # close the file
             f.close()
 
